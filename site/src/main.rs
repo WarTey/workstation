0865a8497461e6ec29b66dd771226b48774cea92
Guillaume Blanc de Lanaute <guillaume.blanc-de-lanaute@isen.yncrea.fr>
2020-04-18 11:47:27 +0200

    Ajout d'un exemple Tera de SergioBenitez

--- site/src/main.rs
+++ site/src/main.rs
@@ -1,12 +1,45 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
 #[macro_use] extern crate rocket;
+#[macro_use] extern crate serde_derive;
+
+use std::collections::HashMap;
+
+use rocket::Request;
+use rocket::response::Redirect;
+use rocket_contrib::templates::Template;
+
+#[derive(Serialize)]
+struct TemplateContext {
+    name: String,
+    items: Vec<&'static str>
+}
 
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn index() -> Redirect {
+    Redirect::to(uri!(get: name = "Unknown"))
+}
+
+#[get("/hello/<name>")]
+fn get(name: String) -> Template {
+    let context = TemplateContext { name, items: vec!["One", "Two", "Three"] };
+    Template::render("index", &context)
+}
+
+#[catch(404)]
+fn not_found(req: &Request<'_>) -> Template {
+    let mut map = HashMap::new();
+    map.insert("path", req.uri().path());
+    Template::render("error/404", &map)
+}
+
+fn rocket() -> rocket::Rocket {
+    rocket::ignite()
+        .mount("/", routes![index, get])
+        .attach(Template::fairing())
+        .register(catchers![not_found])
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket().launch();
 }
