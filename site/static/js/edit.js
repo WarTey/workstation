056e9d12d43fb1a712062c97d6a119e16555f888
Guillaume Blanc de Lanaute <guillaume.blanc-de-lanaute@isen.yncrea.fr>
2020-05-27 18:43:02 +0200

    Suppression d'un 'console.log'

--- site/static/js/edit.js
+++ site/static/js/edit.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
     $('#customSwitch').click(function() {
-        console.log($(this).parents('form').find('input[name="old_password"]'));
         $(this).parents('form').find('input[name="old_password"]').attr("placeholder", $(this)[0].checked ? "Workstation Password" : "Old Password");
     });
 });
